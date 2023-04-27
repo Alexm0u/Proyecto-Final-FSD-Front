1597ce725b01a34c599681afa4cbb355303205a4
Àlex <alexm0u.13@gmail.com>
2023-04-27 12:22:51 +0200

    FEAT: Created profile layout & first attempt with forms on register

--- scoobike-front/src/app/components/NavBar/NavBar.jsx
+++ scoobike-front/src/app/components/NavBar/NavBar.jsx
@@ -3,16 +3,17 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
 export const NavBar = () => {
   return (
     <>
     <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Scoobike</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/login">Login</Nav.Link>
+            <Nav.Link href="/about">Sobre Nosotros</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
