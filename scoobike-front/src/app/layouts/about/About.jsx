75a2a1621f5b0e2647463c1b843574687e0a48bc
Àlex <alexm0u.13@gmail.com>
2023-05-02 16:21:26 +0200

    FEAT: Creating cards for diferent services

--- scoobike-front/src/app/layouts/about/About.jsx
+++ scoobike-front/src/app/layouts/about/About.jsx
@@ -6,49 +6,70 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 
 
+
 export const About = () => {
   return (
     <div className="repairs-container">
-      <div className="repairs-header" style={{gap: '2rem'}}>
-        <span className='stroke-text'>TRAER TU PATINETE O BICI A NUESTRO TALLER</span>
+      <div className="repairs-header">
+        <h2 className='stroke-text'>SERVICIOS PARA PATINETE:</h2>
       </div>
-      <div className="cards-services">
-      <Card className='card' style={{ width: '18rem' }}>
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-      <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+      <div className="cards-services" >
+        <Card className='card'>
+          <Card.Body>
+            <Card.Title>REVISIÓN TOTAL</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Revisamos el patinete en su totalidad para garantizar que puedas circular sin problemas.
+            </ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Button variant="primary">Solicitar cita</Button>
+          </Card.Body>
+        </Card>
 
-    <Card className='card' style={{ width: '18rem' }}>
-      
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-      <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
-    </Card>
+        <Card className='card'>
+          <Card.Body>
+            <Card.Title>REPARACIÓN HOLGURA</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Reparamos la holgura del manillar para evitar que se parta en algún bache. 
+            Esto también mejora la sensación de fluidez mientras circulas y dejarás de escuchar ese "cracketeo" 
+            tan molesto.
+            </ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Button variant="primary">Solicitar cita</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className='card'>
+          <Card.Body>
+            <Card.Title>RUEDA: HINCHADO O RECAMBIO</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Si la presión de las ruedas no es óptima, podemos perder adherencia y exponernos a un accidente. 
+              En Scoobike ajustaremos la presión para que todo vaya sobre ruedas y si es necesario realizaremos el cambio por una nueva.
+            </ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Button variant="primary">Solicitar cita</Button>
+          </Card.Body>
+        </Card>
+
+        <Card className='card'>
+          <Card.Body>
+            <Card.Title>INSTALACIÓN DE SUSPENSIÓN</Card.Title>
+          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>Si notas demasiado esos pequeños baches y te resultan excesívamente incómodos podemos instalar
+              una suspensión que haga tu trayecto mucho mas confortable. No vas a creer que vas en patinete cuando lo pruebes!
+            </ListGroup.Item>
+          </ListGroup>
+          <Card.Body>
+            <Button variant="primary">Solicitar cita</Button>
+          </Card.Body>
+        </Card>
+
 
       </div>
     </div>
