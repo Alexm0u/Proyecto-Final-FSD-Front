f010d1dd44768568b571d19e9f199aa07b4f8fe7
Àlex <alexm0u.13@gmail.com>
2023-05-02 12:27:38 +0200

    FEAT: Cards on home not responsive, deleted and back to imgs

--- scoobike-front/src/app/components/ourServicesSection/OurServices.jsx
+++ scoobike-front/src/app/components/ourServicesSection/OurServices.jsx
@@ -3,8 +3,6 @@ import imgServ1 from '../../../img/ourserv1.jpeg'
 import imgServ2 from '../../../img/ourserv2.jpeg'
 import imgServ3 from '../../../img/ourserv3.jpeg'
 import './ourServices.css'
-import Card from 'react-bootstrap/Card';
-import ListGroup from 'react-bootstrap/ListGroup';
 
 
 const OurServices = () => {
@@ -14,68 +12,10 @@ const OurServices = () => {
 
         <h1 className='header-serv'>Consulta sobre nuestros servicios de mantenimiento y reparación</h1>
         <div className="servicios-img">
-        <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={imgServ1} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={imgServ2} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-
-    <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={imgServ3} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-          {/* <img className='serv-img' src= alt="imagen serv 1" /> */}
-          {/* <img className='serv-img' src= alt="imagen serv 2" /> */}
-          {/* <img className='serv-img' src= alt="imagen serv 3" /> */}
+          
+          <img className='serv-img' src={imgServ1} alt="imagen serv 1" />
+          <img className='serv-img' src={imgServ2} alt="imagen serv 2" />
+          <img className='serv-img' src={imgServ3} alt="imagen serv 3" />
         </div>
 
 
--- scoobike-front/src/app/layouts/about/About.jsx
+++ scoobike-front/src/app/layouts/about/About.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './About.css'
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 
 
@@ -7,11 +9,64 @@ export const About = () => {
   return (
     <div className="repairs-container">
       <div className="repairs-header" style={{gap: '2rem'}}>
-        <span className='stroke-text'>TRAER TU</span>
-        <span>PATINETE O BICI</span>
-        <span className='stroke-text'>A NUESTRO TALLER</span>
+        <span className='stroke-text'>TRAER TU PATINETE O BICI A NUESTRO TALLER</span>
       </div>
-      <div className="cards-services"></div>
+      <div className="cards-services">
+      <Card className='card'>
+      <Card.Header>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Header className='card-header'>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Header className='card-header'>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Header className='card-header'>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+
+    <Card className='card'>
+      <Card.Header className='card-header'>Featured</Card.Header>
+      <Card.Body>
+        <Card.Title>Special title treatment</Card.Title>
+        <Card.Text>
+          With supporting text below as a natural lead-in to additional content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+      </div>
     </div>
   )
 }
