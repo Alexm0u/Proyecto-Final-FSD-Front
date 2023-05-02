7cd2dd635e3ad264bc4eb6888c701b7879034f7d
Àlex <alexm0u.13@gmail.com>
2023-05-02 11:50:18 +0200

    FEAT: Changed imgs for cards on Home

--- scoobike-front/src/app/layouts/about/About.jsx
+++ scoobike-front/src/app/layouts/about/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import './About.css'
 
 
 
 export const About = () => {
   return (
-    <>
-    <div>Estás en la página About</div>
-    </>
+    <div className="repairs-container">
+      <div className="repairs-header" style={{gap: '2rem'}}>
+        <span className='stroke-text'>TRAER TU</span>
+        <span>PATINETE O BICI</span>
+        <span className='stroke-text'>A NUESTRO TALLER</span>
+      </div>
+      <div className="cards-services"></div>
+    </div>
   )
 }
