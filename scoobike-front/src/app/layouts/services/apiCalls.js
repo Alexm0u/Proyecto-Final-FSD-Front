3d5ab4e1d04c52f749adaa77ff5dff54c2dd00ed
Àlex <alexm0u.13@gmail.com>
2023-05-04 18:02:40 +0200

    FEAT:Trying to geall appointments and users as admin for modify

--- scoobike-front/src/app/layouts/services/apiCalls.js
+++ scoobike-front/src/app/layouts/services/apiCalls.js
@@ -61,3 +61,20 @@ export const userUpdate = async (body, token) => {
     };
   return await axios.put(`${root}/user/update/`, body, config)
 }
+export const changeRole = async (body, token) => {
+  const { id, role_id } = body;
+  // const userId = req.params.id
+  const bodyParameters = {
+    id: id,
+    role_id: role_id,
+          //       where: {
+          // id: userId
+          // }
+    };
+    
+  const config = {
+    headers: { 
+      'Authorization': 'Bearer '+ token,  
+    }}
+  return await axios.put(`${root}/user/update/role/.id`,  bodyParameters, config);
+}
