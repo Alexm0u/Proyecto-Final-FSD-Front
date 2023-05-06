a70591a2a23405742c3f7cd0f3de8b8b837bce5e
Àlex <alexm0u.13@gmail.com>
2023-05-06 10:38:51 +0200

    FEAT: Erased video from home (so heavy to charge) and first version of readme

--- scoobike-front/src/app/components/hero/HeroSection.jsx
+++ scoobike-front/src/app/components/hero/HeroSection.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import vidHero from '../../../img/vidHero.mp4'
+// import vidHero from '../../../img/vidHero.mp4'
 import './HeroSection.css'
 
 
 const HeroSection = () => {
   return (
     <div className='hero-container'>
-        <video className='vidHero' src={vidHero} autoPlay loop muted />
+        <img src="../../img/backgroundhome.jpg" alt="fondo imagen" />
         <h1>Una nueva forma de moverse</h1>
         <p>económica, sostenible y perfecta para la ciudad</p>
 
