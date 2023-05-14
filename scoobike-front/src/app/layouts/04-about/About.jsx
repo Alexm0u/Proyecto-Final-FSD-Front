37030166d676cdf4a98ef61e1d1bf7cd1033e9a7
Àlex <alexm0u.13@gmail.com>
2023-05-14 17:33:05 +0200

    FIX: Fixed error consoles on About for classnames

--- scoobike-front/src/app/layouts/04-about/About.jsx
+++ scoobike-front/src/app/layouts/04-about/About.jsx
@@ -9,11 +9,11 @@ import Button from 'react-bootstrap/Button';
 export const About = () => {
   return (
     <div className="repairs-container">
-      <div class="card">
-        <div class="card-header">
-          <h1 class="card-title">Servicios para Patinete</h1>
+      <div className="card">
+        <div className="card-header">
+          <h1 className="card-title">Servicios para Patinete</h1>
         </div>
-        <div class="card-body">
+        <div className="card-body">
           <li>Revisión completa</li>
           <li>Reparación holgura del manillar</li>
           <li>Cambio de rueda</li>
@@ -23,22 +23,22 @@ export const About = () => {
           <li>Recambios por rotura</li>
 
         </div>
-        <div class="card-footer">
+        <div className="card-footer">
           <Button className='boton-cita' variant="primary" href='/login'>Solicita cita</Button>
         </div>
       </div>
-      <div class="card">
-        <div class="card-header">
-          <h1 class="card-title">Servicios para Bicicleta</h1>
+      <div className="card">
+        <div className="card-header">
+          <h1 className="card-title">Servicios para Bicicleta</h1>
         </div>
-        <div class="card-body">
+        <div className="card-body">
           <li>Revisión completa</li>
           <li>Reparación y revisión frenos (disco o pastilla)</li>
           <li>Cambio de rueda o reparación pinchazo</li>
           <li>Hinchado ruedas</li>
           <li>Recambios por rotura</li>
         </div>
-        <div class="card-footer">
+        <div className="card-footer">
           <Button className='boton-cita' variant="primary" href='/login'>Solicita cita</Button>
         </div>
       </div>
