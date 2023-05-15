4c9ddacbdcd0f613a656496f6fdcd7ebecaddd1e
Àlex <alexm0u.13@gmail.com>
2023-05-15 15:52:27 +0200

    FEAT: Chaning background for Home (better quality)

--- scoobike-front/src/app/layouts/00-home/Home.jsx
+++ scoobike-front/src/app/layouts/00-home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Home.css'
 import OurServices from '../../components/ourServicesSection/OurServices'
 import { Col, Container, Row } from 'react-bootstrap'
-import Imgbck from '../../../img/bikeherosection.jpg'
+import Imgbck from '../../../img/back4k.jpg'
 
 
 
@@ -14,21 +14,18 @@ export const Home = () => {
   return (
     <>
       <Container fluid>
-        
+
         <Row className='Row-hero'>
-        <h1 className='text-center'>
-          Bienvenido a Scoobike
-        </h1>
+          <h1 className='text-center'>
+            Bienvenido a Scoobike
+          </h1>
           <Col className='col-main' lg={12} xs={10}>
             <img className='bck-img-home' src={Imgbck}></img>
-
-
-
           </Col>
         </Row>
       </Container>
       <OurServices />
-      
+
 
     </>
   )
