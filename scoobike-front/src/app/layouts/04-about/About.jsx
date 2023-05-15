7a25ea692bafa4f10f72dd52cc3b4ee56b2f6c64
Àlex <alexm0u.13@gmail.com>
2023-05-15 12:24:54 +0200

    FEAT: Fixing cards on about

--- scoobike-front/src/app/layouts/04-about/About.jsx
+++ scoobike-front/src/app/layouts/04-about/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './About.css'
 import Button from 'react-bootstrap/Button';
+import { Col, Container, Row } from 'react-bootstrap';
 
 
 
@@ -8,41 +9,50 @@ import Button from 'react-bootstrap/Button';
 
 export const About = () => {
   return (
-    <div className="repairs-container minheight">
-      <div className="card">
-        <div className="card-header">
-          <h1 className="card-title">Servicios para Patinete</h1>
-        </div>
-        <div className="card-body">
-          <li>Revisión completa</li>
-          <li>Reparación holgura del manillar</li>
-          <li>Cambio de rueda</li>
-          <li>Hinchado ruedas</li>
-          <li>Revisión frenos</li>
-          <li>Instalación amortiguación</li>
-          <li>Recambios por rotura</li>
+    <Container>
+      <Row>
 
-        </div>
-        <div className="card-footer">
-          <Button className='boton-cita' variant="primary" href='/login'>Solicita cita</Button>
-        </div>
-      </div>
-      <div className="card">
-        <div className="card-header">
-          <h1 className="card-title">Servicios para Bicicleta</h1>
-        </div>
-        <div className="card-body">
-          <li>Revisión completa</li>
-          <li>Reparación y revisión frenos (disco o pastilla)</li>
-          <li>Cambio de rueda o reparación pinchazo</li>
-          <li>Hinchado ruedas</li>
-          <li>Recambios por rotura</li>
-        </div>
-        <div className="card-footer">
-          <Button className='boton-cita' variant="primary" href='/login'>Solicita cita</Button>
-        </div>
-      </div>
+      <Col className='repairs-container' lg={6} xs={12}>
 
-    </div>
+
+        <div className="card">
+          <div className="card-header">
+            <h1 className="card-title">Servicios para Bicicleta</h1>
+          </div>
+          <div className="card-body">
+            <li>Revisión completa</li>
+            <li>Reparación y revisión frenos (disco o pastilla)</li>
+            <li>Cambio de rueda o reparación pinchazo</li>
+            <li>Hinchado ruedas</li>
+            <li>Recambios por rotura</li>
+          </div>
+          <div className="card-footer">
+            <Button className='boton-cita' variant="primary" href='/login'>Solicita cita</Button>
+          </div>
+        </div>
+
+      </Col>
+      <Col className='repairs-container'lg={6} xs={12}>
+        <div className="card">
+          <div className="card-header">
+            <h1 className="card-title">Servicios para Patinete</h1>
+          </div>
+          <div className="card-body">
+            <li>Revisión completa</li>
+            <li>Reparación holgura del manillar</li>
+            <li>Cambio de rueda</li>
+            <li>Hinchado ruedas</li>
+            <li>Revisión frenos</li>
+            <li>Instalación amortiguación</li>
+            <li>Recambios por rotura</li>
+
+          </div>
+          <div className="card-footer">
+            <Button className='boton-cita' variant="primary" href='/login'>Solicita cita</Button>
+          </div>
+        </div></Col>
+    </Row></Container>
+
+
   )
 }
